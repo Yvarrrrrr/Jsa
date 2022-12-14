b40075f72197c7e2ed3c383c67dcdd4dcebeead4
Yvarrrrrr <114744961+Yvarrrrrr@users.noreply.github.com>
2022-12-14 14:56:36 +0100

    Add files via upload

---
+++ Jsa/Jsa/web/script.js
@@ -0,0 +1,18 @@
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+function zetInFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    fruitvak.appendChild(nieuwElement);
+}
+
+function zetInGroenteLa(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    groentevak.appendChild(nieuwElement);
+}
